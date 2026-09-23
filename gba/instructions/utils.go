173c8648956a006f53ae5f8eb8f871eecd9fbd49
xziztt <vigneshmvm2000@gmail.com>
2026-09-23 23:49:44 +0530

    feat: add instruction decode for halfword and sdt

--- gba/instructions/utils.go
+++ gba/instructions/utils.go
@@ -142,3 +142,54 @@ func isPSRTransfer(opcode uint32) bool {
 	// MRS || MSR
 	return isMatch(opcode, 0b0000_1101_1011_1111_1111_1111_1111_1111, 0b0000_0001_0000_1111_0000_0000_0000_0000) || isMatch(opcode, 0b0000_1101_0011_0000_1111_1111_1111_1111, 0b0000_0001_0010_0000_1111_1111_1111_1111)
 }
+
+//Memory: Halfword, Doubleword, and Signed Data Transfer
+//   Bit    Expl.
+//   31-28  Condition
+//   27-25  Must be 000b for this instruction
+//   24     P - Pre/Post (0=post; add offset after transfer, 1=pre; before trans.)
+//   23     U - Up/Down Bit (0=down; subtract offset from base, 1=up; add to base)
+//   22     I - Immediate Offset Flag (0=Register Offset, 1=Immediate Offset)
+//   When above Bit 24 P=0 (Post-indexing, write-back is ALWAYS enabled):
+//     21     Not used, must be zero (0)
+//   When above Bit 24 P=1 (Pre-indexing, write-back is optional):
+//     21     W - Write-back bit (0=no write-back, 1=write address into base)
+//   20     L - Load/Store bit (0=Store to memory, 1=Load from memory)
+//   19-16  Rn - Base register                (R0-R15) (Including R15=PC+8)
+//   15-12  Rd - Source/Destination Register  (R0-R15) (Including R15=PC+12)
+//   11-8   When above Bit 22 I=0 (Register as Offset):
+//            Not used. Must be 0000b
+//          When above Bit 22 I=1 (immediate as Offset):
+//            Immediate Offset (upper 4bits)
+//   7      Reserved, must be set (1)
+//   6-5    Opcode (0-3)
+//          When Bit 20 L=0 (Store) (and Doubleword Load/Store):
+//           0: Reserved for SWP instruction
+//           1: STR{cond}H  Rd,<Address>  ;Store halfword   [a]=Rd
+//           2: LDR{cond}D  Rd,<Address>  ;Load Doubleword  R(d)=[a], R(d+1)=[a+4]
+//           3: STR{cond}D  Rd,<Address>  ;Store Doubleword [a]=R(d), [a+4]=R(d+1)
+//          When Bit 20 L=1 (Load):
+//           0: Reserved.
+//           1: LDR{cond}H  Rd,<Address>  ;Load Unsigned halfword (zero-extended)
+//           2: LDR{cond}SB Rd,<Address>  ;Load Signed byte (sign extended)
+//           3: LDR{cond}SH Rd,<Address>  ;Load Signed halfword (sign extended)
+//   4      Reserved, must be set (1)
+//   3-0    When above Bit 22 I=0:
+//            Rm - Offset Register            (R0-R14) (not including R15)
+//          When above Bit 22 I=1:
+//            Immediate Offset (lower 4bits)  (0-255, together with upper bits)
+
+//Double word is not supported by ARM7TDMI
+
+func isHalfWord(opcode uint32) bool {
+	return isMatch(opcode, 0b0000_1110_0000_0000_0000_0000_1001_0000, 0b0000_00000_0000_0000_0000_0000_1001_0000)
+}
+
+// for signed data transfer, we have 2 instructions LDRSB and LDRSH
+// so bit 20 should be set to 1 (store op) and 6-5 should be either 2 or 3 (10 or 11)
+// 4 and 7 are always 1s
+// 27-25 are always 0s
+func isSignedDataTransfer(opcode uint32) bool {
+	return isMatch(opcode, 0b0000_1110_0001_0000_0000_0000_1101_0000, 0b0000_0000_0001_0000_0000_0000_1101_0000)
+
+}
