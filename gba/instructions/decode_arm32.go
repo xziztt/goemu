f879bfe7465d2122642bdf8afd2d8a4de581b891
xziztt <vigneshmvm2000@gmail.com>
2026-09-24 01:20:10 +0530

    feat: added mask for Data Processing instructions

--- gba/instructions/decode_arm32.go
+++ gba/instructions/decode_arm32.go
@@ -231,3 +231,45 @@ func isBlockDataTransfer(opcode uint32) bool {
 func isSingleDataSwap(opcode uint32) bool {
 	return isMatch(opcode, 0b0000_1111_1011_0000_0000_1111_1111_0000, 0b0000_0001_0000_0000_0000_0000_1001_0000)
 }
+
+//   Bit    Expl.
+//   31-28  Condition
+//   27-26  Must be 00b for this instruction
+//   25     I - Immediate 2nd Operand Flag (0=Register, 1=Immediate)
+//   24-21  Opcode (0-Fh)               ;*=Arithmetic, otherwise Logical
+//            0: AND{cond}{S} Rd,Rn,Op2    ;AND logical       Rd = Rn AND Op2
+//            1: EOR{cond}{S} Rd,Rn,Op2    ;XOR logical       Rd = Rn XOR Op2
+//            2: SUB{cond}{S} Rd,Rn,Op2 ;* ;subtract          Rd = Rn-Op2
+//            3: RSB{cond}{S} Rd,Rn,Op2 ;* ;subtract reversed Rd = Op2-Rn
+//            4: ADD{cond}{S} Rd,Rn,Op2 ;* ;add               Rd = Rn+Op2
+//            5: ADC{cond}{S} Rd,Rn,Op2 ;* ;add with carry    Rd = Rn+Op2+Cy
+//            6: SBC{cond}{S} Rd,Rn,Op2 ;* ;sub with carry    Rd = Rn-Op2+Cy-1
+//            7: RSC{cond}{S} Rd,Rn,Op2 ;* ;sub cy. reversed  Rd = Op2-Rn+Cy-1
+//            8: TST{cond}{P}    Rn,Op2    ;test            Void = Rn AND Op2
+//            9: TEQ{cond}{P}    Rn,Op2    ;test exclusive  Void = Rn XOR Op2
+//            A: CMP{cond}{P}    Rn,Op2 ;* ;compare         Void = Rn-Op2
+//            B: CMN{cond}{P}    Rn,Op2 ;* ;compare neg.    Void = Rn+Op2
+//            C: ORR{cond}{S} Rd,Rn,Op2    ;OR logical        Rd = Rn OR Op2
+//            D: MOV{cond}{S} Rd,Op2       ;move              Rd = Op2
+//            E: BIC{cond}{S} Rd,Rn,Op2    ;bit clear         Rd = Rn AND NOT Op2
+//            F: MVN{cond}{S} Rd,Op2       ;not               Rd = NOT Op2
+//   20     S - Set Condition Codes (0=No, 1=Yes) (Must be 1 for opcode 8-B)
+//   19-16  Rn - 1st Operand Register (R0..R15) (including PC=R15)
+//               Must be 0000b for MOV/MVN.
+//   15-12  Rd - Destination Register (R0..R15) (including PC=R15)
+//               Must be 0000b (or 1111b) for CMP/CMN/TST/TEQ{P}.
+//   When above Bit 25 I=0 (Register as 2nd Operand)
+//     When below Bit 4 R=0 - Shift by Immediate
+//       11-7   Is - Shift amount   (1-31, 0=Special/See below)
+//     When below Bit 4 R=1 - Shift by Register
+//       11-8   Rs - Shift register (R0-R14) - only lower 8bit 0-255 used
+//       7      Reserved, must be zero  (otherwise multiply or LDREX or undefined)
+//     6-5    Shift Type (0=LSL, 1=LSR, 2=ASR, 3=ROR)
+//     4      R - Shift by Register Flag (0=Immediate, 1=Register)
+//     3-0    Rm - 2nd Operand Register (R0..R15) (including PC=R15)
+//   When above Bit 25 I=1 (Immediate as 2nd Operand)
+//     11-8   Is - ROR-Shift applied to nn (0-30, in steps o
+
+func isDataProcessing(opcode uint32) bool {
+	return isMatch(opcode, 0b0000_1100_0000_0000_0000_0000_0000_0000, 0b0000_0000_0000_0000_0000_0000_0000_0000)
+}
